eb1857f94699f2b7a6a9393783c1a522ccfbbc7d
bkmxer <ilchuk_anton@mail.ru>
2017-01-11 01:26:23 +0200

    Stoxmarket initial commit

---
+++ app/js/common.js
@@ -0,0 +1,15 @@
+
+$(document).ready(function() {
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:0,
+        nav:false,
+        dots: true,
+        autoplay: false,
+        autoplayTimeout: 9000,
+        items: 1
+    })
+
+});
+
+
