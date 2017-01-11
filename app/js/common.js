21a80c6f123d0ac3068279c2d8b603c282fa62bf
bkmxer <ilchuk_anton@mail.ru>
2017-01-11 18:44:50 +0200

    mockup ready

--- app/js/common.js
+++ app/js/common.js
@@ -4,12 +4,19 @@ $(document).ready(function() {
         loop:true,
         margin:0,
         nav:false,
-        dots: true,
-        autoplay: false,
+        dots: false,
+        autoplay: true,
         autoplayTimeout: 9000,
         items: 1
     })
 
 });
-
-
+/*
+$('label').on('click',function(){
+    if ($('#c2').is(":checked")){
+        console.log('checked');
+        $('.c2Trigger:before').css('backgroundImage', "url('img/svg/checked.svg')");
+        $('.c2Trigger:before').css('backgroundSize', 'cover')
+    }
+})
+*/
